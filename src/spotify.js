70a0ba29c9839b64782e7c405a0d37b6f3171480
carlos <hernandez1989carlos@gmail.com>
2020-11-14 18:33:23 -0500

    create spotify file with all the logig that we gonna use from spotify/ connect Login page with spotify

---
+++ src/spotify.js
@@ -0,0 +1,31 @@
+// https://developer.spotify.com/documentation/web-playback-sdk/quick-start/
+
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+const redirectUri = "http://localhost:3000/";
+
+const clientId = "077a9b72e81c400eaeb4c39018de100c";
+
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state",
+];
+
+export const getTokenFromUrl = () => {
+  return window.location.hash
+    .substring(1)
+    .split("&")
+    .reduce((initial, item) => {
+      let parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+
+      return initial;
+    }, {});
+};
+
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+  "%20"
+)}&response_type=token&show_dialog=true`;
