8419230c25071f3ae4420000c597360ce0174e9b
carlos <hernandez1989carlos@gmail.com>
2020-11-14 19:22:49 -0500

    install spotify-web-api-js in terminal/ create login in App component that connect spotify-web-api-js with our app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Login from "./pages/Login";
+import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
-  return (
-    <div className="app">
-      <Login />
-    </div>
-  );
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then((user) => {
+        console.log("person", user);
+      });
+    }
+
+    console.log("I HAVE A TOKEN >>>", token);
+  }, []);
+
+  return <div className="app">{token ? <h1>I am login</h1> : <Login />}</div>;
 }
 
 export default App;
